359f9076aaf9791f8bbcb7b72ed8b20af33d7761
Vasilij Savin <>
2021-04-17 15:39:46 +0300

    Added check for straight and flush.

--- src/main/java/lt/savin/poker/HandEvaluator.java
+++ src/main/java/lt/savin/poker/HandEvaluator.java
@@ -4,25 +4,38 @@ import lt.savin.poker.model.Card;
 import lt.savin.poker.model.Rank;
 import lt.savin.poker.model.Suit;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
+
+import static lt.savin.poker.PokerApplication.HAND_SIZE;
 
 public class HandEvaluator {
     private static final int FLUSH_SIZE = 5;
+    private static final int STRAIGHT_HAND_DISTANCE = 4;
     Map<Rank, Long> ranksCounters = new HashMap<>();
     Map<Suit, Long> suitCounters = new HashMap<>();
 
-    private void isFlushHand() {
-        List<Map.Entry<Suit, Long>> straight =
+    private int calculateRankDistance(List<Long> rankList) {
+        return (int) (rankList.get(rankList.size()-1) - rankList.get(0));
+    }
+
+    private boolean isStraightHand() {
+        List<Long> rankList = new ArrayList<>(ranksCounters.keySet()).stream()
+                .map(Rank::getValue)
+                .sorted()
+                .collect(Collectors.toList());
+
+        return (calculateRankDistance(rankList) == STRAIGHT_HAND_DISTANCE && rankList.size() == HAND_SIZE);
+    }
+
+    private boolean isFlushHand() {
+        // Looking if any suit repeats 5 times within a hand.
+        List<Map.Entry<Suit, Long>> flush =
                 suitCounters.entrySet().stream()
                         .filter(suit -> suit.getValue().intValue() == FLUSH_SIZE)
                         .collect(Collectors.toList());
 
-        if (straight.size() > 0) {
-            System.out.println(straight.get(0));
-        }
+        return flush.size() > 0;
     }
 
     public HandEvaluator(List<Card> playerHand) {
@@ -32,5 +45,7 @@ public class HandEvaluator {
         });
 
         isFlushHand();
+        isStraightHand();
+        //TODO edge case, when ACE equals one
     }
 }
