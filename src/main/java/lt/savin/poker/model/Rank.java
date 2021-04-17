fdd16c557af4ba5c3afceb63f350f802a2ff918a
Vasilij Savin <>
2021-04-17 15:38:58 +0300

    Added more values, as I realised I need them for calculations.

--- src/main/java/lt/savin/poker/model/Rank.java
+++ src/main/java/lt/savin/poker/model/Rank.java
@@ -23,11 +23,19 @@ public enum Rank {
 
     Rank(char code) {
         switch (code) {
-            case 'T': this.value = 10;
-            case 'J': this.value = 11;
-            case 'Q': this.value = 12;
-            case 'K': this.value = 13;
-            case 'A': this.value = 14;
+            case '2' -> this.value = 2;
+            case '3' -> this.value = 3;
+            case '4' -> this.value = 4;
+            case '5' -> this.value = 5;
+            case '6' -> this.value = 6;
+            case '7' -> this.value = 7;
+            case '8' -> this.value = 8;
+            case '9' -> this.value = 9;
+            case 'T' -> this.value = 10;
+            case 'J' -> this.value = 11;
+            case 'Q' -> this.value = 12;
+            case 'K' -> this.value = 13;
+            case 'A' -> this.value = 14;
         }
     }
 }
